c6a86f378df512c10e2e03b7811e8b822efc464e
Shawon-Mahmud07 <shawon.phero@gmail.com>
2026-01-12 23:44:22 +0600

    implement JavaScript toggle for responsive navigation menu

--- JS/app.js
+++ JS/app.js
@@ -1,1 +1,7 @@
+const navToggle = document.getElementById("navToggle");
+const navLinks = document.getElementById("navLinks");
 
+navToggle.addEventListener("click", () => {
+  navToggle.classList.toggle("active");
+  navLinks.classList.toggle("active");
+});
